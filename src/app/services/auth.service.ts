c47957a0335df277c3c42c2d1c4e0361eab37cbb
ilyaneya <ilyaneya@mail.ru>
2020-10-12 01:06:53 +0300

    init

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {IAuth, IAuthResponse} from '../interface/auth.interface';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class AuthService{
+  constructor(private http: HttpClient) {
+  }
+  public authorization(data: IAuth): Observable<IAuthResponse> {
+    return this.http.post<IAuthResponse>('http://emphasoft-test-assignment.herokuapp.com/api-token-auth/', data);
+
+  }
+  public logout(): void {
+    localStorage.removeItem('token');
+  }
+}
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IUser} from '../interface/user.interface';
+
+@Injectable()
+export class UsersService{
+  constructor(private http: HttpClient ) {
+  }
+  public getUsers(): Observable<IUser[]>{
+    return this.http.get<IUser[]>('http://emphasoft-test-assignment.herokuapp.com/api/v1/users/');
+  }
+}
